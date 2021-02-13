4944a8956ab5a64ee39011846fa60afe617c399c
Janne Mattila <janne.mattila@hotmail.fi>
2021-02-13 21:03:09 +0200

    Add powershell module placeholders

---
+++ src/MyChess.psm1
@@ -0,0 +1,35 @@
+function Connect-MyChess
+(
+) {
+    throw [System.NotImplementedException]::new()
+}
+
+function Disconnect-MyChess
+(
+) {
+    throw [System.NotImplementedException]::new()
+}
+
+function Get-MyChessGame
+(
+) {
+    throw [System.NotImplementedException]::new()
+}
+
+function New-MyChessGameMove
+(
+) {
+    throw [System.NotImplementedException]::new()
+}
+
+function Get-MyChessFriend
+(
+) {
+    throw [System.NotImplementedException]::new()
+}
+
+function New-MyChessGame
+(
+) {
+    throw [System.NotImplementedException]::new()
+}
