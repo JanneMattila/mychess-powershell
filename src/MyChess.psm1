1e154dc7d2ea43c851a992acb2ebb0d67206f882
Janne Mattila <janne.mattila@hotmail.fi>
2021-02-15 18:23:25 +0200

    Change to global variables

--- src/MyChess.psm1
+++ src/MyChess.psm1
@@ -6,16 +6,17 @@ function Connect-MyChess
 (
     [Parameter(HelpMessage = "My Chess environment")] 
     [ValidateSet("Development", "Production")]
-    [string] $Environment = "Production"
+    [string] 
+    $Environment = "Production"
 ) {
     $environments = @{ 
         "Development" = @{
-            Address    = "https://mychess-dev.jannemattila.com";
+            Address    = "https://azfun-mychess-z67p7cxye4n5q.azurewebsites.net";
             AppId      = "f4617c7d-a6c2-444a-b7c0-a9942dd88b3c";
             ResourceId = "9ea259c1-4a8c-489a-86d5-54fa90dc3fd3"
         };
         "Production"  = @{
-            Address    = "https://mychess.jannemattila.com";
+            Address    = "https://azfun-mychess-dvcszl3mb4xx4.azurewebsites.net";
             AppId      = "6a824c79-d2d8-4bd0-8696-f2c0c7487d7b";
             ResourceId = "52cec0e5-b1db-4a0d-bd51-dcffb7c67959"
         };
@@ -27,6 +28,11 @@ function Connect-MyChess
     $tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/token"
 
     $authResponse = Invoke-RestMethod -Uri $authEndpoint
+    try {
+        # Windows only for now.
+        $authResponse.user_code | clip
+    }
+    catch {}
     Write-Host $authResponse.message
 
     $tokenPayload = "resource=$($selectedEnvironment.ResourceId)&client_id=$($selectedEnvironment.AppId)&grant_type=device_code&code=$($authResponse.device_code)"
@@ -34,30 +40,39 @@ function Connect-MyChess
     $accessToken = ""
     while ($true) {
         try {
-            Start-Sleep -Seconds $deviceCodeResponse.interval
             $tokenEndpointResponse = Invoke-RestMethod -Uri $tokenEndpoint -Method POST -Body $tokenPayload
             if ($null -ne $tokenEndpointResponse.access_token) {
                 $accessToken = $tokenEndpointResponse.access_token
                 break
             }
+            Start-Sleep -Seconds $authResponse.interval
         }
         catch {}
     }
 
-    $env:MYCHESS_ADDRESS = $environments[$Environment].Address
-    $env:MYCHESS_TOKEN = $accessToken
+    $global:MYCHESS = @{
+        "Address" = $environments[$Environment].Address
+        "Token"   = $accessToken
+    }
 }
 
 function Disconnect-MyChess
 (
 ) {
-    throw [System.NotImplementedException]::new()
+    Remove-Variable MYCHESS -Scope Global -Force
 }
 
 function Get-MyChessGame
 (
+    [Parameter(HelpMessage = "Game state")] 
+    [ValidateSet("WaitingForYou", "WaitingForOpponent", "Archive")]
+    [string] 
+    $GameState = "WaitingForYou"
 ) {
-    throw [System.NotImplementedException]::new()
+    $uri = $global:MYCHESS.Address + "/api/games?state=" + $GameState
+    $token = ConvertTo-SecureString -String $global:MYCHESS.Token -AsPlainText
+    $games = Invoke-RestMethod -Uri $uri -Authentication Bearer -Token $token
+    $games
 }
 
 function New-MyChessGameMove
